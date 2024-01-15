9989af58387a24b7db140f8c04c0f629dbea8276
Konrad Nowak <wk.k.nowak@gmail.com>
2024-01-15 17:26:14 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import SelectAlgorithm from './components/SelectAlgorithm.vue';
+import SelectData from './components/SelectData.vue';
+import ControlPanel from './components/ControlPanel.vue';
+import Sorting from './components/Sorting.vue';
+</script>
+
+<template>
+  <Header />
+  <main>
+    <Sorting />
+    <div class="control-wrapper">
+      <SelectAlgorithm />
+      <SelectData />
+      <ControlPanel />
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+@import './assets/scss/variables';
+
+main {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+</style>
